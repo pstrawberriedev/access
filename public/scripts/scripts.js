ddfe441cfc8bf5556e3c2d7011d0b4c2b0d4358a
unknown <josh.depaolo@01DePaoloJ.wsmcorp.com>
2016-05-20 13:41:02 -0600

    a little more content and styling updates, some better spacing in html

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -3,5 +3,5 @@
 console.log('--> scripts.js');
 
 // Ally
-console.log('loaded ally.js in version', ally.version);
+console.log('loaded ally.js v', ally.version);
 console.log('focusable elements', ally.query.focusable());
