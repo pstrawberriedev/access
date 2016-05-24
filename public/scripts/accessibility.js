09b0356d82c46ce0db11cb2195d6440558b19258
pstrawt4 <me@josh.care>
2016-05-23 18:07:08 -0600

    implemented reusable tab-muting function - USE WITH CARE. updated accordion functionality to use tab muting as a specific use case for this page

--- public/scripts/accessibility.js
+++ public/scripts/accessibility.js
@@ -2,18 +2,57 @@
 
 console.log('--> accessibility.js');
 
+/*---------------
+  #Utility
+---------------*/
+
+// Reusable Function: Check aria-hidden status
+function ariaCheck(ele) {
+  return ele.attr('aria-hidden');
+}
+
+// Disable child elements from being tabbed
+// @parent: pass in parent element
+// @type: in types of child elements to be enabled
+// **WARNING** This can break tabbability if not used properly
+function disableChildTabbing(parent, type) {
+  parent.find(type).attr('tabindex', '-1');
+}
+
+// Re-enable child element(s) tabbability
+// @parent: in parent element
+// @type: in types of child elements to be enabled
+// **WARNING** This can break tabbability if not used properly
+function enableChildTabbing(parent, type) {
+  
+  if(type = $('input')) {
+    parent.find(type).removeAttr('tabindex');
+  } else {
+    parent.find(type).attr('tabindex', '1');
+  }
+  
+}
 
 /*---------------
   #Accordions
 ---------------*/
 
 var accordion = $('.access-accordion');
+var accordionInner = $('.access-accordion [role=tabpanel]');
+
+// Check for aria-hidden status and disable tabbing appropriately
+  $(document).ready(function() {
+    if(ariaCheck(accordionInner) === 'true') {
+      disableChildTabbing(accordionInner, $('input'))
+    }
+  });
 
 accordion.each(function() {
-
+  
   // Accordion Variables
   var self = $(this);
-  var trigger = $(this).find('a[role=tab]');
+  var trigger = $(this).find('[role=tab]');
+  var triggerArrow = $(this).find('[role=tab] span');
   var inner = $(this).find('[role=tabpanel]');
 
   // Reusable Function: Open Accordion
@@ -21,6 +60,8 @@ accordion.each(function() {
     TweenLite.set(inner, { height: "auto" });
     TweenLite.from(inner, 0.2, { height: 0 });
     inner.attr('aria-hidden', 'false'); // toggle aria-hidden
+    triggerArrow.removeClass('typcn-arrow-sorted-down');
+    triggerArrow.addClass('typcn-arrow-sorted-up');
   }
 
   // Reusable Function: Close Accordion
@@ -28,13 +69,10 @@ accordion.each(function() {
     TweenLite.set(inner, { height: "auto" });
     TweenLite.to(inner, 0.2, { height: 0 });
     inner.attr('aria-hidden', 'true'); // toggle aria-hidden
+    triggerArrow.removeClass('typcn-arrow-sorted-up');
+    triggerArrow.addClass('typcn-arrow-sorted-down');
   }
-
-  // Reusable Function: Check aria-hidden status
-  function ariaCheck(ele) {
-    return ele.attr('aria-hidden');
-  }
-
+  
   // Trigger Action (Open or Close Accordion)
   trigger.on('click', function(e) {
     
@@ -42,8 +80,10 @@ accordion.each(function() {
     
     if(ariaCheck(inner) === 'true') {
       openAccordion();
+      enableChildTabbing(inner, $('input'));
     } else {
       closeAccordion();
+      disableChildTabbing(inner, $('input'));
     }
 
   });
